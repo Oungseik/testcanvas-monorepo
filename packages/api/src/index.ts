f4b2fa84014df0a9f16229643082b3fe7844799f
Min Aung Thu Win <mhemaungthuwin@gmail.com>
2024-12-28 08:30:58 +0630

    feat: add api implementations

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,3 +1,4 @@
 import { HttpApi } from "@effect/platform";
+import { AuthApi, CheckHealthApi } from "./Api";
 
-export const Api = HttpApi.make("AuthApi");
+export const Api = HttpApi.make("AuthApi").add(CheckHealthApi).add(AuthApi);
