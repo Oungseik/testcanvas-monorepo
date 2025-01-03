9059a2a28e3c7e427c582602ee4bd1c8a7b4116a
Min Aung Thu Win <mhemaungthuwin@gmail.com>
2025-01-03 11:25:38 +0630

    chore(flake): add the dependencies and env vars

--- flake.nix
+++ flake.nix
@@ -18,14 +18,18 @@
         devShell = pkgs.mkShell {
           buildInputs = with pkgs; [
             android-tools
+            android-studio
             biome
             bun
+            ffmpeg
             openjdk21
             python3
           ];
 
           shellHook = '''';
           LD_LIBRARY_PATH = "${pkgs.stdenv.cc.cc.lib}/lib";
+          JAVA_HOME = "${pkgs.openjdk21}/lib/openjdk";
+          ANDROID_HOME = "$HOME/Android/Sdk";
         };
       }
     );
