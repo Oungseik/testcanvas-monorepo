82ee3ea968464860c0ff331210ba0774803f1524
Min Aung Thu Win <mhemaungthuwin@gmail.com>
2025-01-07 15:43:34 +0630

    fix: configure the ios provider properly
    In the last version, the IosProvider is configure for only android.
    We fix that.

--- apps/agent/src/Services/GADS.ts
+++ apps/agent/src/Services/GADS.ts
@@ -6,14 +6,16 @@
  *
  * */
 import { HttpBody, HttpClient } from "@effect/platform";
-import { AndroidProvider, type IosProvider } from "@repo/domain";
+import { AndroidProvider, IosProvider } from "@repo/domain";
 import { Config, Effect as Ef, Schema as S } from "effect";
 import getPort, { portNumbers } from "get-port";
 
 type Provider = AndroidProvider | IosProvider;
 type OS = "linux" | "windows" | "darwin";
 
-function setupProvider(nickname: string) {
+// FIXME - this function contains a lot of duplication
+export function setupAndroidProvider() {
+  const nickname = "AndroidProvider";
   return Ef.gen(function* () {
     const client = yield* HttpClient.HttpClient;
     const gadsHost = yield* Config.string("GADS_URL");
@@ -49,5 +51,51 @@ function setupProvider(nickname: string) {
   });
 }
 
-export const setupAndroidProvider = () => setupProvider("AndroidProvider");
-export const setupIosProvider = () => setupProvider("IosProvider");
+// FIXME - this function contains a lot of duplication
+export function setupIosProvider() {
+  const nickname = "IosProvider";
+
+  return Ef.gen(function* () {
+    const client = yield* HttpClient.HttpClient;
+    const gadsHost = yield* Config.string("GADS_URL");
+
+    const wda_bundle_id = yield* Config.string("WDA_BUNDLE_ID");
+    const wda_repo_path = yield* Config.string("WDA_REPO_PATH");
+    const use_custom_wda = yield* Config.boolean("USE_CUSTOM_WDA");
+    const supervision_password = yield* Config.string("SUPERVISION_PASSWORD").pipe(Config.withDefault(undefined));
+
+    const providers = (yield* client
+      .get(`${gadsHost}/admin/providers`)
+      .pipe(Ef.andThen((res) => res.json))) as Provider[];
+
+    if (providers.some((p) => p.nickname === nickname)) {
+      return yield* Ef.logInfo(`${nickname} already exist`);
+    }
+
+    const selenium_grid = yield* Config.string("SELENIUM_GRID_URL");
+    const host_address = yield* Config.string("HOST_ADDRESS");
+    const os = (yield* Config.string("OS")) as OS;
+    const port = yield* Ef.promise(() => getPort({ port: portNumbers(10000, 12000) }));
+
+    const provider = yield* S.decode(IosProvider)({
+      nickname,
+      os,
+      port,
+      supervision_password, 
+      provide_ios: true,
+      wda_bundle_id,
+      wda_repo_path,
+      use_custom_wda, 
+      use_selenium_grid: true,
+      selenium_grid,
+      host_address,
+    });
+    const providerBody = yield* HttpBody.json(provider);
+    yield* client
+      .post(`${gadsHost}/admin/providers/add`, {
+        body: providerBody,
+      })
+      .pipe(Ef.tap(Ef.logInfo(`successfully setup ${nickname}`)));
+  });
+
+}
