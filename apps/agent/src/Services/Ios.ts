12547bfae136de2bdfe35b3024a73be79f4f7265
Min Aung Thu Win <mhemaungthuwin@gmail.com>
2025-01-07 12:09:42 +0630

    refactor: fix lint error and format the code

--- apps/agent/src/Services/Ios.ts
+++ apps/agent/src/Services/Ios.ts
@@ -1,8 +1,8 @@
-import { type Udid } from "@repo/domain";
 import { IosTracker } from "@/Trackers";
-import { FetchHttpClient, HttpBody, HttpClient } from "@effect/platform";
-import {Config, Effect as Ef  } from "effect";
 import { getInfo } from "@/Trackers/Ios";
+import { FetchHttpClient, HttpBody, HttpClient } from "@effect/platform";
+import type { Udid } from "@repo/domain";
+import { Config, Effect as Ef } from "effect";
 
 /** keep device udid with the model name just for logging purpose */
 const deviceMap: Map<Udid, string> = new Map();
@@ -11,26 +11,27 @@ function addIos(udid: Udid) {
   return Ef.gen(function* () {
     const gadsHost = yield* Config.string("GADS_URL");
     const client = yield* HttpClient.HttpClient;
-    const {name, os_version} = yield* getInfo(udid)
+    const { name, os_version } = yield* getInfo(udid);
 
     yield* HttpBody.json({
-       udid : udid,
-       device_type : "real",
-       os : "ios",
-       provider : "IosProvider",
-       usage : "enabled",
-       name,
-       os_version,
+      udid: udid,
+      device_type: "real",
+      os: "ios",
+      provider: "IosProvider",
+      usage: "enabled",
+      name,
+      os_version,
 
-      // TODO replace this with the correct value 
-       screen_height : "680",
-       screen_width : "375",
-    }).pipe(Ef.andThen(body => client.post(`${gadsHost}/admin/device`, { body })),
-      Ef.andThen(res => res.json),
+      // TODO replace this with the correct value
+      screen_height: "680",
+      screen_width: "375",
+    }).pipe(
+      Ef.andThen((body) => client.post(`${gadsHost}/admin/device`, { body })),
+      Ef.andThen((res) => res.json),
       Ef.tap(() => Ef.succeed(deviceMap.set(udid, name ?? "unknown"))),
-      Ef.tap(Ef.logInfo(`connect device ${name ?? udid}`))
-    )
-  })
+      Ef.tap(Ef.logInfo(`connect device ${name ?? udid}`)),
+    );
+  });
 }
 
 function removeIos(udid: Udid) {
@@ -42,16 +43,14 @@ function removeIos(udid: Udid) {
     yield* client.del(`${gadsHost}/admin/device/${udid}`).pipe(
       Ef.andThen((res) => res.json),
       Ef.tap(Ef.logInfo(`disconnect device ${name ?? udid}`)),
-    )
-
-  })
+    );
+  });
 }
 
 export function trackAndSetupIos() {
-  IosTracker.subscribe(async ({event, device: {id}} ) => {
-
+  IosTracker.subscribe(async ({ event, device: { id } }) => {
     if (event === "add") {
-      const task = addIos(id as Udid)
+      const task = addIos(id as Udid);
       await task.pipe(Ef.scoped, Ef.provide(FetchHttpClient.layer), Ef.runPromise);
     }
 
@@ -60,6 +59,5 @@ export function trackAndSetupIos() {
       // TODO handle the error of the task
       await task.pipe(Ef.scoped, Ef.provide(FetchHttpClient.layer), Ef.runPromise);
     }
-
-  })
+  });
 }
--- apps/agent/src/Trackers/Ios.ts
+++ apps/agent/src/Trackers/Ios.ts
@@ -15,37 +15,34 @@ class IosTracker extends Signal<IosEvent> {
     super();
 
     const ps = cp.spawn("ios", ["listen"]);
-    ps.stdout.on("data", (data) =>
-      data
-        .toString()
-        .split("\n")
-        .filter((output: string) => output) // since there might be new line, we need to remove empty string
-        .forEach((output: string) => {
-          const device = JSON.parse(output);
-          if (device.MessageType === "Attached") {
-            this.notify({
-              event: "add",
-              device: { id: device.Properties.SerialNumber as Udid, type: "real" },
-            });
-          }
+    ps.stdout.on("data", (data) => {
+      for (const output of data.toString().split("\n")) {
+        if (!output) {
+          continue;
+        }
+        const device = JSON.parse(output);
+        if (device.MessageType === "Attached") {
+          this.notify({
+            event: "add",
+            device: { id: device.Properties.SerialNumber as Udid, type: "real" },
+          });
+        }
 
-          if (device.Message === "Detached") {
-            this.notify({ event: "remove", device: { id: "unknown" as Udid, type: "real" } });
-          }
-        }),
-    );
+        if (device.Message === "Detached") {
+          this.notify({ event: "remove", device: { id: "unknown" as Udid, type: "real" } });
+        }
+      }
+    });
   }
 }
 
 export const tracker = new IosTracker();
 
-
 export class GoIosError extends Data.TaggedError("GoIosError")<{
-  readonly message: string
+  readonly message: string;
 }> {}
 
-
-interface IphoneInfo  {
+interface IphoneInfo {
   name: string;
   os_version: string;
 }
@@ -53,21 +50,26 @@ interface IphoneInfo  {
 export function getInfo(udid: Udid) {
   return Ef.async<IphoneInfo, GoIosError>((resume) => {
     cp.exec(`ios info --udid=${udid}`, (err, stdout, stderr) => {
-
       if (err) {
-        return resume(new GoIosError({message: err.message}))
+        return resume(new GoIosError({ message: err.message }));
       }
 
       if (stderr) {
-        return resume(new GoIosError({message: JSON.parse(stderr).msg ?? "Something went wrong while reading the ios device info" }))
+        return resume(
+          new GoIosError({
+            message:
+              JSON.parse(stderr).msg ?? "Something went wrong while reading the ios device info",
+          }),
+        );
       }
 
       const data = JSON.parse(stdout);
-      return resume(Ef.succeed({
-        name: data["DeviceName"],
-        os_version: data["ProductVersion"]
-      }))
-    })
-    
-  })
+      return resume(
+        Ef.succeed({
+          name: data.DeviceName,
+          os_version: data.ProductVersion,
+        }),
+      );
+    });
+  });
 }
