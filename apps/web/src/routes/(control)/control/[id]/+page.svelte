dcfcb207ef48d12b8c4d809b96ad31a89167959c
Min Aung Thu Win <mhemaungthuwin@gmail.com>
2025-01-08 12:11:44 +0630

    feat: create device screen

---
+++ apps/web/src/routes/(control)/control/[id]/+page.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+</script>
+
+<div class="grid grid-cols-3 min-h-dvh">
+  <div class="col-span-2 mx-auto my-8">
+    <div>
+      <div class="display">
+        <!-- svelte-ignore a11y_no_static_element_interactions -->
+        <div
+          class="artboard artboard-demo phone-2 border-4 border-base-content">
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="flex flex-col bg-base-200 shadow-lg">
+    <h2 class="text-xl font-bold p-4">Appium Logs</h2>
+    <pre class="flex-grow bg-base-300 p-4 h-[calc(100dvh-60px)] overflow-auto text-sm">
+      "this is logging"
+    </pre>
+  </div>
+</div>
